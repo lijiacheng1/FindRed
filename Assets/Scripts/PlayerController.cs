6e7b5c8c1be14db2e7ae77ace3d3afeb9d629213
Prinny <601239484@qq.com>
2019-01-26 09:59:49 +0800

    Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    Rigidbody2D rig;
 	// Use this for initialization
 	void Start () {
-		
+        rig = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        var h =Input.GetAxis("Horizontal");
+        var v =Input.GetAxis("Vertical");
+        rig.MovePosition((Vector2)transform.position + new Vector2(h * Time.deltaTime,v * Time.deltaTime));
+    }
 }
