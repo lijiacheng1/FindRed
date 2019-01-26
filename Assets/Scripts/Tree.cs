4f6ecb20d70a1a13f58ed26af15b5336a5a6a94b
lijiacheng1 <32295849+lijiacheng1@users.noreply.github.com>
2019-01-26 21:31:04 +0800

    修改树叶

--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tree : MonoBehaviour {
+public class Tree : MonoBehaviour,InteractivityObject
+{
+    public GameObject leaf;
+    public GameObject ship;
+    public GameObject riverCollider;
     private SpriteRenderer sr;
-    private Color colorRed = new Color(252, 96, 106, 255);
+    private Color colorRed = new Color((float)252/255, (float)96/255, (float)106/255, 1);
+    private int pressRem;
     private void Start()
     {
         sr = GetComponent<SpriteRenderer>();
     }
     public void ChangeColor(int pressCounter)
     {
-        sr.color = Color.Lerp(Color.white, colorRed, 1);
+        sr.material.color = Color.Lerp(Color.white, colorRed, (float)pressCounter/2);
+        pressRem = pressCounter; 
+    }
+    public int PressE()
+    {
+        Debug.Log(pressRem);
+        if (pressRem == 0)
+        {
+            riverCollider.SetActive(false);
+            ship.SetActive(true);
+        }
+        if (pressRem == 2)
+        {
+            leaf.SetActive(true);
+        }
+        return 0;
     }
 }
