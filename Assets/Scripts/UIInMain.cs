5e60cd1c5c716777df57c498d95e3075437bf265
Prinny <601239484@qq.com>
2019-01-26 17:10:03 +0800

    00006

--- Assets/Scripts/UIInMain.cs
+++ Assets/Scripts/UIInMain.cs
@@ -6,21 +6,98 @@ using DG.Tweening;
 
 
 public class UIInMain : MonoBehaviour {
-
+    public static UIInMain instacne;
     public Image title;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Image wasd;
+    public Image e;
+    public Transform environment;
+    public Transform playerCutscene;
+    public Transform player;
 
-    public void Cutscene()
+    Animator playerCutsceneAnim;
+    //Animator playerAnim;
+    SpriteRenderer[] srs;
+
+    bool canE = false;
+    bool canWasd = false;
+
+    private void Awake()
+    {
+        if (instacne == null)
+            instacne = this;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        srs = environment.GetComponentsInChildren<SpriteRenderer>();
+        playerCutsceneAnim = playerCutscene.GetComponent<Animator>();
+        //playerAnim = player.GetComponent<Animator>();
+
+        for (int i = 0; i < srs.Length; i++)
+        {
+            srs[i].material.DOFade(0, 0);
+        }
+        environment.gameObject.SetActive(false);
+        wasd.gameObject.SetActive(false);
+        e.gameObject.SetActive(false);
+        player.gameObject.SetActive(false);
+        playerCutscene.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (canE && Input.GetKeyDown(KeyCode.E))
+        {
+            canE = false;
+            e.gameObject.SetActive(false);
+            Cutscene2();
+        }
+        if (canWasd && (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) ||
+            Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D)))
+        {
+            canWasd = false;
+            wasd.gameObject.SetActive(false);
+            //Cutscene3();
+        }
+    }
+
+    public void Cutscene1()
     {
         title.DOFade(0, 1);
+        environment.gameObject.SetActive(true);
+        for (int i = 0; i < srs.Length; i++)
+        {
+            srs[i].material.DOFade(1, 1);
+        }
+        playerCutsceneAnim.SetTrigger("Start");
+    }
+
+    public void InputCutScene1()
+    {
+        e.DOFade(0, 0);
+        e.gameObject.SetActive(true);
+        e.DOFade(1, 1);
+        canE = true;
+    }
+
+    public void Cutscene2()
+    {
+        playerCutsceneAnim.SetTrigger("GetUp");
+    }
+
+    public void InputCutScene2()//get up end
+    {
+        wasd.DOFade(0, 0);
+        wasd.gameObject.SetActive(true);
+        wasd.DOFade(1, 1);
+        player.gameObject.SetActive(true);
+        playerCutscene.gameObject.SetActive(false);
+        canWasd = true;
+    }
+
+    public void Cutscene3()
+    {
     }
 
     public void LoadScene()
