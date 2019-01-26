e6b072f6cb19a764d2d429037ba2b5bda5f7ee94
Prinny <601239484@qq.com>
2019-01-26 10:05:09 +0800

    Test

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,7 +12,7 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        var h =Input.GetAxis("Horizontal");
+        var h = Input.GetAxis("Horizontal");
         var v =Input.GetAxis("Vertical");
         rig.MovePosition((Vector2)transform.position + new Vector2(h * Time.deltaTime,v * Time.deltaTime));
     }
