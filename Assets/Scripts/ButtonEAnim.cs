40e802967ac95ee2df047c45bf88bd5a3fc45631
Prinny <601239484@qq.com>
2019-01-27 11:54:46 +0800

    00017

--- Assets/Scripts/ButtonEAnim.cs
+++ Assets/Scripts/ButtonEAnim.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using DG.Tweening;
 
 public class ButtonEAnim : MonoBehaviour {
+    Image img;
+    float fadeTime = 2;
 
-	// Use this for initialization
-	void OnEnable () {
+
+    public void Start()
+    {
+        img = GetComponent<Image>();
+    }
+    // Use this for initialization
+    void OnEnable () {
 	}
 	
 	// Update is called once per frame
 	void OnDisable () {
 		
 	}
+
+    public void Show()
+    {
+        img.DOFade(0, 0).OnComplete(() => img.gameObject.SetActive(true));
+        img.DOFade(1, fadeTime);
+    }
+
+    public void Hide()
+    {
+        img.DOFade(0, fadeTime).OnComplete(() => img.gameObject.SetActive(false));
+    }
 }
