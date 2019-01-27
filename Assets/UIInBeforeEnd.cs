74e060ca805b9022b0c39684510f5df720a19143
Prinny <601239484@qq.com>
2019-01-27 12:14:26 +0800

    00018

---
+++ Assets/UIInBeforeEnd.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIInBeforeEnd : MonoBehaviour
+{
+    public static UIInBeforeEnd instacne;
+    public Transform environment;
+    public Transform playerCutscene;
+
+    Animator playerCutsceneAnim;
+    //Animator playerAnim;
+    SpriteRenderer[] srs;
+
+    float fadeTime = 2f;
+
+    private void Awake()
+    {
+        if (instacne == null)
+            instacne = this;
+    }
+    // Use this for initialization
+    void Start()
+    {
+        srs = environment.GetComponentsInChildren<SpriteRenderer>();
+        playerCutsceneAnim = playerCutscene.GetComponent<Animator>();
+
+        for (int i = 0; i < srs.Length; i++)
+        {
+            srs[i].material.DOFade(0, 0);
+        }
+        environment.gameObject.SetActive(false);
+        playerCutscene.gameObject.SetActive(false);
+
+        Cutscene1();
+    }
+
+    void Cutscene1()
+    {
+        playerCutscene.gameObject.SetActive(true);
+        environment.gameObject.SetActive(true);
+        for (int i = 0; i < srs.Length; i++)
+        {
+            srs[i].material.DOFade(1, fadeTime);
+        }
+        StartCoroutine(Cutscene1a());
+    }
+
+    IEnumerator Cutscene1a()
+    {
+        yield return new WaitForSeconds(2);
+        playerCutsceneAnim.SetTrigger("Start");
+    }
+}
